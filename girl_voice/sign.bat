e8436cf8fc8c09db6160cd8cda4d9f4f4ee05c72
quantumlike <quantumlike@users.noreply.github.com>
2017-06-08 23:10:32 -0400

    Add files via upload

---
+++ girl_voice/sign.bat
@@ -0,0 +1,5 @@
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+cd /d %mypath%
+
+"E:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\signtool.exe"  sign /a /s MY /t http://timestamp.verisign.com/scripts/timstamp.dll /v girl_voice.exe
