621be3bd65b1c46db268f7cc2acfaf633f72c378
quantumlike <quantumlike@users.noreply.github.com>
2017-06-08 23:10:57 -0400

    Add files via upload

--- man_voice/sign.bat
+++ man_voice/sign.bat
@@ -2,4 +2,4 @@ SET mypath=%~dp0
 echo %mypath:~0,-1%
 cd /d %mypath%
 
-"E:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\signtool.exe"  sign /fMyCert.pfx /t http://timestamp.verisign.com/scripts/timstamp.dll MyControl.exe
+"E:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\signtool.exe"  sign /a /s MY /t http://timestamp.verisign.com/scripts/timstamp.dll /v man_voice.exe
