d22cb4bf6f29625d11b26ec21a4c6353c150c078
quantumlike <quantumlike@users.noreply.github.com>
2017-06-10 14:30:40 -0400

    Create sign.bat

--- man_voice/sign.bat
+++ man_voice/sign.bat
@@ -2,4 +2,4 @@ SET mypath=%~dp0
 echo %mypath:~0,-1%
 cd /d %mypath%
 
-"E:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\signtool.exe"  sign /a /s MY /t http://timestamp.verisign.com/scripts/timstamp.dll /v man_voice.exe
+"E:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\signtool.exe"  sign /a /s MY /t http://timestamp.verisign.com/scripts/timstamp.dll /v boy_voice.exe
